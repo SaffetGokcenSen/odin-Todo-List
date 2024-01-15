ae2e3130ce3f89b0fa4af5de81eaf9096b3da0dd
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-01-15 20:52:47 +0300

    factory function to create project objects

---
+++ src/projectModule.js
@@ -0,0 +1,8 @@
+// A factory function to create a project object. 
+// projectObjectName: The name of the project entered by the user. 
+// projectContent: The todo lists contained by the project.
+function createProject(projectObjectName) {
+    // the content of the project is initially nothing. 
+    const projectContent = [];
+    return { projectObjectName, projectContent };
+} 
