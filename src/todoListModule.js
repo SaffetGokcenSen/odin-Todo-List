659277db49db9110629e1a455cd5a35cb13a646e
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-01-15 21:04:31 +0300

    function to save the todo list to the local storage

--- src/todoListModule.js
+++ src/todoListModule.js
@@ -1,3 +1,5 @@
+import { addTodoListName } from "./projectModule.js";
+
 // the factory function to create a todo list. 
 // storageName: The name of the object in the localStorage, set by the programmer. 
 // title: The title of the todo list, entered by the user.
@@ -6,4 +8,13 @@
 // priority: The priority of the todo list, entered by the user.
 function createTodoList(storageName, title, description, dueDate, priority) {
     return { storageName, title, description, dueDate, priority };
+}
+
+// the function to save a todo list in a project. 
+// theTodoObject: The todo object to be saved in the local storage. 
+// theProjectName: The name of the project the content of which is to be updated 
+// by adding the new todo list.
+function saveTodoList(theTodoObject, theProjectName) {
+    localStorage.setItem(theTodoObject.storageName, JSON.stringify(theTodoObject)); 
+    addTodoListName(theProjectName, theTodoObject.storageName);
 }
