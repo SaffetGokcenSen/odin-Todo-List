79738efd8b1063c555e22896260db408377adaff
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-01-23 00:34:42 +0300

    check for the existance of existingProjectNames

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 // the functions for dealing with todo lists and related projects are now to be 
 // tested using the console. 
+
+// Check if the object "existingProjectNames" exists on the local storage. If it 
+// exists, fetch it from the local storage. Otherwise, create it and store it in 
+// the local storage. 
+if (!localStorage.getItem("existingProjectNames")) {
+    const existingProjectNames = {
+        projectNames: [],
+    };
+    console.log("No existingProjectNames object in the local storage!"); 
+    localStorage.setItem("existingProjectNames", JSON.stringify(existingProjectNames));
+} 
+else {
+    console.log("existingProjectNames object in the local storage!");
+    const existingProjectNames = JSON.parse(localStorage.getItem("existingProjectNames"));
+} 
