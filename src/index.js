595c54f8fdbfae849083b81f75281111f1be4f5c
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-01-24 23:36:58 +0300

    a sample todo created and saved

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-// the functions for dealing with todo lists and related projects are now to be 
+import { createTodoList, saveTodoList } from "./todoListModule";
+// The functions for dealing with todo lists and related projects are now to be 
 // tested using the console. 
 
 // Check if the object "existingProjectNames" exists on the local storage. If it 
@@ -15,3 +16,13 @@ else {
     console.log("existingProjectNames object in the local storage!");
     const existingProjectNames = JSON.parse(localStorage.getItem("existingProjectNames"));
 } 
+
+// Assume that a new todo list is created and saved in the local storage
+title = "The school fee"; 
+descripton = "I have to pay my son's school fee.";
+dueDate = "02.01.1943";
+prioriy = "high"; 
+ownerProject = "MySon";
+todo = createTodoList(title, description, dueDate, priority, ownerProject); 
+storageName = ownerProject + "-" + title;
+saveTodoList(todo, storageName, ownerProject);
