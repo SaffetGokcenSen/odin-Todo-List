7fcd2213a1a04c6958103f0c3a519c95def7d5d2
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-01-24 22:38:45 +0300

    a parameter of createTodoList removed

--- src/todoListModule.js
+++ src/todoListModule.js
@@ -1,13 +1,12 @@
 import { addTodoListName, removeTodoListName } from "./projectModule.js";
 
 // the factory function to create a todo list. 
-// storageName: The name of the object in the localStorage, set by the programmer. 
 // title: The title of the todo list, entered by the user.
 // description: The description of the todo list, entered by the user. 
 // dueDate: The due date of the todo list, entered by the user. 
 // priority: The priority of the todo list, entered by the user. 
 // ownerProject: The project which owns the todo list.
-function createTodoList(storageName, title, description, dueDate, priority, 
+function createTodoList(title, description, dueDate, priority, 
     ownerProject) {
     return { storageName, title, description, dueDate, priority, ownerProject };
 }
