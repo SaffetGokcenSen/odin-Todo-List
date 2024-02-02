3558c99717524a86dcbf60d77a16f4bf35522f9c
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-02-02 11:56:33 +0300

    cons declarations added

--- src/todoListModule.js
+++ src/todoListModule.js
@@ -1,4 +1,4 @@
-import { addTodoListName, removeTodoListName } from "./projectModule.js";
+import { removeTodoListName } from "./projectModule.js";
 
 // the factory function to create a todo list. 
 // title: The title of the todo list, entered by the user.
@@ -28,7 +28,7 @@ function saveTodoList(theTodoObject, storageName, theProjectName) {
 // the function to get a todo list 
 // todoStorageName: The storage name of the todo list 
 function getTodo(todoStorageName) {
-    tempObject = JSON.parse(localStorage.getItem(todoStorageName)); 
+    const tempObject = JSON.parse(localStorage.getItem(todoStorageName)); 
     return tempObject;
 }
 
@@ -36,7 +36,7 @@ function getTodo(todoStorageName) {
 // todoStorageName: The storage name of the todo list 
 // newTitle: The new title of the todo list
 function updateTodoListTitle(todoStorageName, newTitle) {
-    tempObject = getTodo(todoStorageName);
+    const tempObject = getTodo(todoStorageName);
     tempObject.title = newTitle; 
     putTodo(todoStorageName, tempObject);
 } 
@@ -45,7 +45,7 @@ function updateTodoListTitle(todoStorageName, newTitle) {
 // todoStorageName: The storage name of the todo list 
 // newTitle: The new description of the todo list
 function updateTodoListDescription(todoStorageName, newDescription) {
-    tempObject = getTodo(todoStorageName);
+    const tempObject = getTodo(todoStorageName);
     tempObject.description = newDescription; 
     putTodo(todoStorageName, tempObject);
 } 
@@ -54,7 +54,7 @@ function updateTodoListDescription(todoStorageName, newDescription) {
 // todoStorageName: The storage name of the todo list 
 // newDueDate: The new due date of the todo list
 function updateTodoListDueDate(todoStorageName, newDueDate) {
-    tempObject = getTodo(todoStorageName);
+    const tempObject = getTodo(todoStorageName);
     tempObject.dueDate = newDueDate; 
     putTodo(todoStorageName, tempObject);
 } 
@@ -63,7 +63,7 @@ function updateTodoListDueDate(todoStorageName, newDueDate) {
 // todoStorageName: The storage name of the todo list 
 // newPriority: The new priority of the todo list
 function updateTodoListPriority(todoStorageName, newPriority) {
-    tempObject = getTodo(todoStorageName);
+    const tempObject = getTodo(todoStorageName);
     tempObject.priority = newPriority; 
     putTodo(todoStorageName, tempObject);
 } 
@@ -71,8 +71,8 @@ function updateTodoListPriority(todoStorageName, newPriority) {
 // the function to delete a todo list 
 // todoStorageName: The storage name of the todo list 
 function deleteTodoList(todoStorageName) { 
-    tempObject = getTodo(todoStorageName);
-    ownerProject = tempObject.ownerProject; 
+    const tempObject = getTodo(todoStorageName);
+    const ownerProject = tempObject.ownerProject; 
     localStorage.removeItem(todoStorageName); 
     removeTodoListName(ownerProject, todoStorageName);
 } 
