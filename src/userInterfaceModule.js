d9451d7fbadaa656b5ec15231e29e9cd2eb384be
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-03-04 21:24:32 +0300

    user window is created

--- src/userInterfaceModule.js
+++ src/userInterfaceModule.js
@@ -1,4 +1,7 @@
 // the function for setting up the main frames of the user interface. 
 function setupMainFrames() {
-    
-}
+    // the window with which the user is going to interact is created.
+    const userWindow = document.createElement("div"); 
+} 
+
+export { setupMainFrames };
