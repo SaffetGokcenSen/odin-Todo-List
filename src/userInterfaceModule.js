11d1475d56c49bd9c44f62c153cfa4a94ee25c21
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-03-12 22:13:04 +0300

    userWindow is a flex container

--- src/userInterfaceModule.js
+++ src/userInterfaceModule.js
@@ -1,7 +1,11 @@
+import './style.css';
+
 // the function for setting up the main frames of the user interface. 
 function setupMainFrames() {
     // the window with which the user is going to interact is created.
     const userWindow = document.createElement("div"); 
+    // the class of the user window is userWindow
+    userWindow.className = "userWindow"; 
 } 
 
 export { setupMainFrames };
