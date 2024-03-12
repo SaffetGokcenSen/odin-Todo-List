bb489e1c07662dc232bb821fe069f87c639e1e30
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-03-12 22:24:43 +0300

    comment added

--- src/userInterfaceModule.js
+++ src/userInterfaceModule.js
@@ -6,6 +6,7 @@ function setupMainFrames() {
     const userWindow = document.createElement("div"); 
     // the class of the user window is userWindow
     userWindow.className = "userWindow"; 
+    // userWindow is added to the page
     document.body.appendChild(userWindow);
 } 
 
