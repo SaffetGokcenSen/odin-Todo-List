04be9f0f9bdadea66f038c53805cb2aaab9a8fda
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-03-13 20:52:31 +0300

    project names window added to the user window

--- src/userInterfaceModule.js
+++ src/userInterfaceModule.js
@@ -7,7 +7,14 @@ function setupMainFrames() {
     // the class of the user window is userWindow
     userWindow.className = "userWindow"; 
     // userWindow is added to the page
-    document.body.appendChild(userWindow);
+    document.body.appendChild(userWindow); 
+
+    // the window in which project names are listed 
+    const projectNamesWindow = document.createElement("div"); 
+    // the class of the project names window is projectNamesWindow 
+    projectNamesWindow.className = "projectNamesWindow"; 
+    // project names window is added to the user window
+    userWindow.appendChild(projectNamesWindow);
 } 
 
 export { setupMainFrames };
