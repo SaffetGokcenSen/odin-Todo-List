534443991963d1ecf0861ba490408764a317a481
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2024-04-09 12:23:57 +0300

    todo name is in a span element

--- src/todoWindowModule.js
+++ src/todoWindowModule.js
@@ -2,7 +2,7 @@ import { getTodo } from "./todoListModule";
 
 function printTodo() {
     // the name of the project which owns the todo
-    const projectName = this.parentNode.firstChild.textContent; 
+    const projectName = this.parentNode.parentNode.firstChild.textContent; 
     // the name of the todo
     const todoName = this.textContent; 
     // the storage name of the todo
